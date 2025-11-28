525ca1498ee419d13abb58e68f424f4d7178dea6
Manu S Ajith <neo@codingarena.in>
2025-11-29 00:17:39 +0300

    Update development environment configuration

--- devenv.nix
+++ devenv.nix
@@ -6,22 +6,15 @@
     package = pkgs.beam.packages.erlang_28.elixir_1_19;
   };
 
-  languages.erlang = {
-    enable = true;
-    package = pkgs.beam.interpreters.erlang_28;
-  };
-
   packages = [
     pkgs.git
   ];
 
   enterShell = ''
-    echo "Langfuse Elixir SDK development environment"
-    echo "Elixir: $(elixir --version | head -1)"
-    echo "Erlang: $(erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().' -noshell 2>&1)"
+    export MIX_OS_DEPS_COMPILE_PARTITION_COUNT=$(nproc || 0)
   '';
 
-  pre-commit.hooks = {
+  git-hooks.hooks = {
     mix-format = {
       enable = true;
       name = "mix format";
